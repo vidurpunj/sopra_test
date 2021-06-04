9de8d6313de5a005a8dc67c2ae793108cb36e35d
neerajkumar1989 <neeraj@stepforwardsolution.com>
2021-06-04 21:51:46 +0530

    Seed data for users

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
-# README
+##
+1. Create new Rails 6.0.3.7 aap with ruby 2.6.5p114,
+   mysql database.
+2. Added Jquery, bootstrap and poppoer.js
+3. Added seed required users.
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users data
+User.create({ name: 'Sam Pedro', email: 'sam@email.com', password: 12345678 })
+User.create({ name: 'Peter Troll', email: 'peter@email.com', password: 12345678 })
+User.create({ name: 'Chris Bla', email: 'chris@email.com', password: 12345678 })
+User.create({ name: 'Suzan Something', email: 'suzan@email.com', password: 12345678 })
+User.create({ name: 'Axel Ontheway', email: 'alex@email.com', password: 12345678 })
+User.create({ name: 'Mack Boe', email: 'mack@email.com', password: 12345678 })
+User.create({ name: 'Rik van der Kroon', email: 'rik@email.com', password: 12345678 })
