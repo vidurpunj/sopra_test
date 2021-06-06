07776cac825a52a46ab78a279284c9862ae10144
neerajkumar1989 <neeraj@stepforwardsolution.com>
2021-06-06 22:35:40 +0530

    Add the reviews tests

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -5,7 +5,7 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   include Warden::Test::Helpers
 
-  test 'Redirect to the series page with flash message if no params' do
+  test 'TV Series: Redirect to the series page with flash message if no params' do
     @user = users(:vidur)
     sign_in(@user)
     post '/tv_series'
@@ -13,12 +13,28 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     assert_equal "Please provide a file to be uploaded !", flash[:alert]
   end
 
-  test "upload TV series data with params" do
+  test "TV Series: upload TV series data with params" do
     @user = users(:vidur)
     sign_in(@user)
-    post '/tv_series', params: { file: fixture_file_upload('files/reviews.csv','text/csv')}
+    post '/tv_series', params: { file: fixture_file_upload('files/tv_series.csv','text/csv')}
     assert_response :redirect
     assert_equal "TV series uploaded!", flash[:notice]
+  end
+
+  test 'Reviews: Redirect to the series page with flash message if no params' do
+    @user = users(:punj)
+    sign_in(@user)
+    post '/tv_seriel_comments'
+    assert_response :redirect
+    assert_equal "Please provide a file to be uploaded !", flash[:alert]
+  end
+
+  test "Reviews: upload data with params" do
+    @user = users(:punj)
+    sign_in(@user)
+    post '/tv_seriel_comments', params: { file: fixture_file_upload('files/reviews.csv','text/csv')}
+    assert_response :redirect
+    assert_equal "TV series comment uploaded!", flash[:notice]
   end
 
 end
